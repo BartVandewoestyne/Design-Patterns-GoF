f6c5184efe503ab691ecc041245300ba4e9b2ba1
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-12-05 15:58:30 +0100

    * Splitted Coord.h from Point.h

---
+++ Foundation_Classes/Coord.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include "Coord.h"
+
+Coord min(Coord x, Coord y)
+{
+    return min(x, y);
+}
+
+Coord max(Coord x, Coord y)
+{
+    return max(x, y);
+}
+
+Coord abs(Coord x)
+{
+    return abs(x);
+}
+
+int round(Coord x)
+{
+    return floor(x + 0.5);
+}
---
+++ Foundation_Classes/Coord.h
@@ -0,0 +1,11 @@
+#ifndef COORD_H
+#define COORD_H
+
+typedef float Coord;
+
+Coord min(Coord x, Coord y);
+Coord max(Coord x, Coord y);
+Coord abs(Coord x);
+int round(Coord x);
+
+#endif /* COORD_H */
