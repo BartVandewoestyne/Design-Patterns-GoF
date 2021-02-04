517f8112a7d135c0048a32941137d8f4cd21f893
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-02-04 22:36:36 +0100

    Add TODO about potential problem with the MySingleton class.

--- Creational_Patterns/Singleton/registry_of_singletons/main.cpp
+++ Creational_Patterns/Singleton/registry_of_singletons/main.cpp
@@ -8,6 +8,15 @@
 
 int main()
 {
+    // TODO: commenting this line in shows that it is still possible to
+    // create instances of MySingleton using its default constructor.  If
+    // we try to avoid that by making the MySingleton() constructor protected
+    // instead of public, then the line
+    //     static MySingleton theSingleton;
+    // in MySingleton.cpp gives an error at compile time:
+    //   error C2248: 'MySingleton::MySingleton': cannot access protected member declared in class 'MySingleton'
+    //MySingleton foo;
+
     Singleton* mySingleton= MySingleton::Instance();
 
     Singleton::Register("MySingleton", mySingleton);
