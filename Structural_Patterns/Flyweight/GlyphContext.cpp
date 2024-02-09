738cd33423b3207a2d5271421b0427a28471da38
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2024-02-09 16:04:31 +0100

    Add TODO about how GlyphContext::GetFont() should be implemented.
    The GoF book has some info on how to implement GlyphContext::GetFont()
    on page 202-203.  I added this info as a TODO-comment.

--- Structural_Patterns/Flyweight/GlyphContext.cpp
+++ Structural_Patterns/Flyweight/GlyphContext.cpp
@@ -18,7 +18,12 @@ void GlyphContext::Insert(int quantity1)
 
 Font* GlyphContext::GetFont()
 {
-    // TODO
+    // TODO: GlyphContext::GetFont() uses the index as a key into a BTree
+    // structure that stores the glyph-to-font mapping.  Each node in the tree
+    // is labeled with the length of the string for which it gives font
+    // information.  Leaves in the tree point to a font, while interior nodes
+    // break the string into substrings, one for each child (see page 202-203
+    // in the book).
     return 0;
 }
 
