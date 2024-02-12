eed58f32485d0b073398c836037912e23298aa22
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2024-02-12 23:21:11 +0100

    Add definitions for the BoundingBox and CreateManipulator methods in Shape.

--- Structural_Patterns/Adapter/Shape.cpp
+++ Structural_Patterns/Adapter/Shape.cpp
@@ -2,3 +2,14 @@
 
 Shape::Shape()
 {}
+
+void Shape::BoundingBox(Point& bottomLeft, Point& topRight) const
+{
+    // TODO
+}
+
+Manipulator* Shape::CreateManipulator() const
+{
+    // TODO
+    return 0;
+}
