a0fe7903894869e6161913266c1c6f9343eaef6f
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2024-02-12 23:59:55 +0100

    Add definitions for several member functions of the Window class.

--- Structural_Patterns/Bridge/Window.cpp
+++ Structural_Patterns/Bridge/Window.cpp
@@ -2,6 +2,38 @@
 #include "WindowImp.h"
 #include "WindowSystemFactory.h"
 
+ void Window::Open() {
+    // TODO
+ }
+ 
+ void Window::Close() {
+    // TODO
+ }
+ 
+ void Window::Iconify() {
+    // TODO
+ }
+ 
+ void Window::Deiconify() {
+    // TODO
+ }
+
+void SetOrigin(const Point& at) {
+    // TODO
+ }
+
+void SetExtent(const Point& extent) {
+    // TODO
+ }
+
+void Raise() {
+    // TODO
+ }
+
+void Lower() {
+    // TODO
+ }
+
 void Window::DrawRect (const Point& p1, const Point& p2) {
     WindowImp* imp = GetWindowImp();
     imp->DeviceRect(p1.X(), p1.Y(), p2.X(), p2.Y());
