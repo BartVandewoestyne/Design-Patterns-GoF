3abfdf89ec1c841e8fc2381a518b79afa89ab7ce
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2024-02-13 22:42:33 +0100

    Add ProgramNode.cpp with definitions for ProgramNode member functions.

---
+++ Structural_Patterns/Facade/ProgramNode.cpp
@@ -0,0 +1,26 @@
+#include "ProgramNode.h"
+
+void ProgramNode::GetSourcePosition(int& line, int& index)
+{
+    // TODO
+}
+
+void ProgramNode::Add(ProgramNode*)
+{
+    // TODO
+}
+
+void ProgramNode::Remove(ProgramNode*)
+{
+    // TODO
+}
+
+void ProgramNode::Traverse(CodeGenerator&)
+{
+    // TODO
+}
+
+ProgramNode::ProgramNode()
+{
+    // TODO
+}
