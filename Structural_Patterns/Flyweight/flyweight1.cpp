4c89f3297a928646da0afcfefa93362b5861f30d
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2024-06-11 23:13:20 +0200

    Add comment.

--- Structural_Patterns/Flyweight/flyweight1.cpp
+++ Structural_Patterns/Flyweight/flyweight1.cpp
@@ -3,6 +3,12 @@
 
 int main()
 {
+
+    // Assuming we're at index 102 in the traversal (see 'BTree structure for
+    // font intormation' in the book page 202), then the following code sets
+    // the font of each character in the word "expect" to that of the
+    // surrounding text (that is, times12, an instance of Font for 12-point
+    // Times Roman).
     GlyphContext gc;
     Font* times12 = new Font("Times-Roman-12");
     Font* timesItalic12 = new Font("Times-Italic-12");
