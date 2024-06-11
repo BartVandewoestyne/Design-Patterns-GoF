80f31c1f09ecee9e9c424dfc0e0ec335eaa38efb
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2024-06-11 23:13:40 +0200

    Minor whitespace change.

--- Structural_Patterns/Flyweight/flyweight1.cpp
+++ Structural_Patterns/Flyweight/flyweight1.cpp
@@ -13,7 +13,6 @@ int main()
     Font* times12 = new Font("Times-Roman-12");
     Font* timesItalic12 = new Font("Times-Italic-12");
     // ...
-    
     gc.SetFont(times12, 6);
 
     gc.Insert(6);
